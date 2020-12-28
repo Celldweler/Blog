f591ddd47528a2b982a2567426e15f22118b7ef9
Raime <0961120962fb@gmail.com>
2020-12-29 00:09:50 +0200

    fix

--- Blog/Controllers/PanelController.cs
+++ Blog/Controllers/PanelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace Blog.Controllers
 {
-    public class PanelController
+    public class PanelController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
